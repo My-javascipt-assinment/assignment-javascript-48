8e5fb5f6bff0db5aa5da968e8a512843a3315e27
Azizullah <azizullahcodes@gmail.com>
2025-08-05 22:27:34 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,304 @@
+// Assignment - 48 
+/* ________________________ Question no 1 _______________________
+Positive Index
+Create an array of 7 colors.
+
+Ask the user to enter a number (0–6).
+
+Use .at() to get the color at that index and display it in the console.
+
+Example Output:
+Enter index: 2
+Color at index 2 is: Green
+___________________________ Answer ______________________________ */
+// let array = ['green','blue','white','red','pink','yellow'];
+// let enterNumber = prompt('Enter a number');
+// console.log(array.at(enterNumber));
+/* ________________________ Question no 2 _______________________
+ Negative Index
+Create an array of 6 fruits.
+
+Ask the user to enter a negative index (like -1 for last element).
+
+Show the fruit at that position using .at().
+
+Example Output:
+
+Enter negative index: -1
+Fruit at -1 is: Mango
+___________________________ Answer ______________________________ */
+// let fruits = ['apple','mango','grapes','banana','guava'];
+// let ind = -2;
+// console.log(fruits.at(ind))
+/* ________________________ Question no 3 _______________________
+ Accessing Last 3 Elements
+Create an array of 10 numbers.
+
+Print the last three elements using .at() with negative indexes.
+
+Example Output:
+
+Last 3 elements: 50, 60, 70
+___________________________ Answer ______________________________ */
+// let num = [10,20,30,40,40,50,60,70,80,90];
+// let last = num.at(-1);
+// let secondLast = num.at(-2);
+// let thirdLast = num.at(-3);
+// let arr = [thirdLast,secondLast,last];
+// console.log(arr)
+/* ________________________ Question no 4 _______________________
+Simple Word Check
+Ask the user to enter a word.
+
+Check if the word starts with the letter "A" using startsWith().
+
+Show a message in the console.
+
+Example Output:
+
+Enter word: Apple
+Yes! It starts with A.
+___________________________ Answer ______________________________ */
+// let word = prompt('enter a word');
+// let check = word.startsWith('A');
+// if(word.startsWith('A')){console.log(`${word} : yes! starts with A`)}
+// else{console.log(`${word} : does not start with A`)}
+/* ________________________ Question no 5 _______________________
+Filter Array Using startsWith()
+Create an array of 8 country names.
+
+Filter and display only those countries starting with the letter "P".
+
+Example Output:
+
+["Pakistan", "Poland", "Philippines"]
+___________________________ Answer ______________________________ */
+// let array = ['indea','newzeland','japan',"Pakistan", "Poland", "Philippines",'Bangladesh'];
+// let need = array.filter(function(element){return element.startsWith('P')})
+// console.log(need)
+/* ________________________ Question no 6 _______________________
+className
+___________________________ Answer ______________________________ */
+// function styling(){let get = document.getElementById('heading');
+//     get.className += ' head';
+// }
+/* ________________________ Question no 7 _______________________
+Create a <p> element in HTML with id="text" and class="highlight".
+
+Using JavaScript, check if the element has a class attribute and print a message in the console.
+
+Example Output:
+
+Yes! This element has a class attribute.
+___________________________ Answer ______________________________ */
+// let get = document.getElementById('text');
+// let check = get.hasAttribute('class');
+// console.log(check);
+// if(check){console.log('ok has attribute')}
+// else{console.log('not has attribute')}
+/* ________________________ Question no 8 _______________________
+ Practice Task for getAttribute()
+Task:
+
+Create an <a> tag with id="link" and href="https://www.google.com".
+
+Using getAttribute(), get the href value and show it in the console.
+
+Example Output:
+
+csharp
+Copy
+Edit
+Link URL is: https://www.google.com
+4. Combined Practice Task
+Task:
+
+Create an <img> tag with id="pic" and src="image.jpg".
+
+Check if it has alt attribute using hasAttribute().
+
+If not, add it using setAttribute('alt', 'Sample Image').
+
+Finally, log the alt value using getAttribute().
+
+If you want, I can create a ready-to-run HTML + JS practice file for these tasks in one page.
+Do you want me to create it?
+
+___________________________ Answer ______________________________ */
+// let get = document.getElementById('pic');
+// let check = get.hasAttribute('alt');
+// console.log(check)
+// get.setAttribute('alt', 'sample Img');
+// let value = get.getAttribute('alt');
+// console.log(value);
+/* ________________________ Question no 9 _______________________
+querySelector
+___________________________ Answer ______________________________ */
+// let get = document.querySelector('p');
+// console.log(get);
+// let get2 = document.querySelectorAll('p');
+// console.log(get2);
+// get2.forEach(function(element){element.style.color = 'blue'})
+/* ________________________ Question no 10 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 11 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 12 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 13 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 14 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 15 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 16 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 17 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 18 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 19 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 20 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 21 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 22 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 23 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 24 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 25 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 26 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 27 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 28 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 29 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 30 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 31 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 32 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 33 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 34 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 35 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 36 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 37 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 38 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 39 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 40 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 41 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 42 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 43 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 44 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 45 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 46 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 47 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 48 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 49 _______________________
+
+___________________________ Answer ______________________________ */
+
+/* ________________________ Question no 50 _______________________
+
+___________________________ Answer ______________________________ */
